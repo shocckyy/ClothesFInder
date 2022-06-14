df3ec9673630763f12ebecc71b7495f64723d60a
sh0cky <98285822+shocckyy@users.noreply.github.com>
2022-06-14 22:20:03 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,61 @@
+﻿import os
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+#the JSON file you downloaded in step 5 above
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'neon-effect-353319-4d5bd10c8943.json'
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+#set this thumbnail as the url
+image = types.Image()
+image.source.image_uri = 'https://i.ytimg.com/vi/UQQHSbeIaB0/maxresdefault.jpg'
+
+# [START vision_logo_detection]
+def detect_logos(path):
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_logo_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    # [END vision_python_migration_logo_detection]
+# [END vision_logo_detection]
+
+
+# [START vision_logo_detection_gcs]
+def detect_logos_uri(uri):
+    """Detects logos in the file located in Google Cloud Storage or on the Web.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+# [END vision_logo_detection_gcs]
